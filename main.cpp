36c2c21a16dfa011444d6a565d45fba868f82250
ReBarbaro <francescovito.dipalo@marconibari.edu.it>
2025-11-20 08:47:01 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int massimo(int n1, int n2){
+  int max;
+  if(n1>n2){
+    max=n1;
+
+    }else{
+        max=n2;
+    }
+    return max;
+   }
+   
+
+
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int n1,n2,n3,n4,max;
+    cout<<"inserisci 4 valori"<<endl;
+    cin>>n1>>n2>>n3>>n4;
+    max=massimo(n1,n2);
+    max=massimo(n3,max);
+    max=massimo(n4,max);
+    cout<<"il valore maggiore è"<<max<<endl;
+    return 0;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
